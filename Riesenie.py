3bd43f48e55997d19a0ea3684e7697c713291151
Samo <Polopoklop741>
2019-02-07 23:26:39 +0100

    Pridanie textu + úpravy

--- Riesenie.py
+++ Riesenie.py
@@ -1,24 +1,22 @@
-f = open("text.txt")
-b = f.read()
-f.close()
-
-text = list(b)
-slovnik = {}
-
-
 def znaky(text):
+    slovnik = {}
     for znak in text:
         if znak in slovnik:
             slovnik[znak] += 1
         else:
             slovnik[znak] = 1
-
     slovnik_usporiadany = dict(sorted(slovnik.items(), key=lambda x: x[1], reverse=True))
-
     zoznam_podla_poctu = []
-
     for k, v in slovnik_usporiadany.items():
         #print(k, v)
         zoznam_podla_poctu.append(k)
-
     return zoznam_podla_poctu
+
+
+f = open("text.txt")
+b = f.read()
+f.close()
+text = list(b)
+
+y = znaky(text)
+print(y)
